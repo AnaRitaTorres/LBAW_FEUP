9f07cd58b9f34a59398915f67f2ce6658fc16d87
AnaRitaTorres <anaritatorres96@outlook.pt>
2017-03-03 17:58:44 +0000

    header resises perfectly

--- Artifacts/A3/header.php
+++ Artifacts/A3/header.php
@@ -11,11 +11,11 @@
     </head>
     <body>
         <header>
-          <nav class="navbar navbar-default navbar-fixed-top">
-            <div class="container-fluid">
+          <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+            <div class="container">
                  <!-- Brand and toggle get grouped for better mobile display -->
                 <div class="navbar-header">
-                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
@@ -27,9 +27,9 @@
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <form class="navbar-form navbar-left">
-                  <div class="col-md-20">
+                  <!--<div class="col-md-20">-->
                     <div id="custom-search-input">
-                        <div class="input-group col-md-20">
+                        <div class="input-group col-md-40">
                             <input type="text" class="form-control input-md" placeholder="Search" />
                             <span class="input-group-btn">
                                 <button class="btn btn-info btn-md" type="button">
@@ -38,7 +38,7 @@
                             </span>
                          </div>
                     </div>
-                 </div>
+                 <!--</div>-->
                 </form>
                 <ul class="nav navbar-nav navbar-right">
                     <li class="dropdown">
@@ -84,4 +84,4 @@
             </div>
             </nav>
      </header>
-     <div class="container">
+     <div class="content">
