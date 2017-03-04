54c64a0794c2d27fb138725c74e8048fee66e738
Miriam Gonçalves <margaitizinha_cbt@hotmail.com>
2017-03-04 17:52:19 +0000

    Revert "header design finished"
    This reverts commit f2127801866393b9735ffab027432576e8af0a33.

--- Artifacts/A3/header.php
+++ Artifacts/A3/header.php
@@ -13,7 +13,7 @@
         <header>
           <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
             <div class="container">
-                <!-- Brand and toggle get grouped for better mobile display -->
+                 <!-- Brand and toggle get grouped for better mobile display -->
                 <div class="navbar-header">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
                     <span class="sr-only">Toggle navigation</span>
@@ -81,7 +81,7 @@
                         </li>
                 </ul>
                 </div>
-               </div>
+            </div>
             </nav>
      </header>
      <div class="content">
